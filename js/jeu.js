69ab7ea358fa8e760366742ab498144278b8cd18
PAYEN Camille <camillepayen35@gmail.com>
2025-11-19 21:31:14 +0100

    The random card function is divided into two distinct parts

--- js/jeu.js
+++ js/jeu.js
@@ -56,7 +56,6 @@ function verifierPaire() {
 
 // Méthode Fisher-Yates Shuffle (échange)
 function photoAleatoire() {
-  var memoPhotos = document.querySelectorAll(".photo");
   var i, j, tmp;
   // Mélange les cartes dans le tableau
   for (i = mesImages.length - 1; i > 0; i--) {
@@ -65,6 +64,11 @@ function photoAleatoire() {
     mesImages[i] = mesImages[j]; // l'index 8 avait l'image 4 elle prend l'image de l'index tiré aléatoirement (index 2 donc image 1)
     mesImages[j] = tmp; // L'index 2 à échangé son image et récupère sa nouvelle image qui était dans le tmp (image 4)
   }
+  afficherCartes();
+}
+
+function afficherCartes() {
+  var memoPhotos = document.querySelectorAll(".photo");
   // Récupère le tableau déjà mélangé donc distribue à chaque .photo dans l'ordre (élément = destination (.photo) et index = index d'une image dans le tableau)
   memoPhotos.forEach((element, index) => {
     element.innerHTML = '<img src="' + mesImages[index] + '" class="w-100" />';
